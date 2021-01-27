992bc4cc0c466d3850e44bbdeed1b23ed051a7cc
Xavier C <chelleri@sheridancollege.ca>
2021-01-27 13:14:46 -0500

    new repo for Student code

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+/*
+ * Xavier Chelleri
+ * Student ID: 991623643
+ * SYST10199 - Web Programming 
+ */
+
+/**
+ *
+ * @author xdcb
+ */
+public class Student {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
