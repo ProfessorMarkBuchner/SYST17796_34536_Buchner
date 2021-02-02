45b9d9ded5137f1410ca432974ab27f83b668eea
zaman <zaman@192.168.0.22>
2021-02-02 12:13:56 -0500

    First Commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,27 @@
+/*
+ * Mehrab Zamanudin
+ * Student ID:
+ * SYST10199 - Web Programming
+ */
+package studentlist;
+
+/**
+ *This class represents students in our application
+ * @author zaman
+ */
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,39 @@
+/*
+ * Mehrab Zamanudin
+ * Student ID:
+ * SYST10199 - Web Programming
+ */
+package studentlist;
+
+import java.util.Scanner;
+
+/**
+ *This class is a simple example of creating arrays of objects
+ * @author zaman
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] students = new Student[2];
+        
+        Scanner input = new Scanner(System.in);
+        
+        for(int i =0; i < students.length; i++){
+            System.out.println("Enter the student's name");
+            String name = input.nextLine();
+            Student student = new Student(name);
+            
+            students[i] = student;
+        }
+        System.out.println("Printing the students:");
+        String format = "The student's name is %s\n";
+        for (Student student: students){
+            System.out.printf(format, student.getName());
+        }
+    }
+    
+}
