200542b56a94b7cd943e3ff795f222f973167e43
loguej <loguej@sheridancollege.ca>
2021-02-16 22:20:12 -0500

    new repo for student code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +22,32 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13) + 1);
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c; 
         }
-        
         //insert code to ask the user for Card value and suit, create their card
+        Scanner k = new Scanner(System.in);
+        System.out.print("Please enter your card value:");
+        int cardValue = k.nextInt();
+        System.out.print("Please enter your suit:");
+        String suit = k.next();
         // and search magicHand here
+        boolean doesItMatch = false;
+        for(int i = 0; i<magicHand.length; i++)
+        {
+            if(magicHand[i].getValue() == cardValue)
+            {
+                if(magicHand[i].getSuit().equals(suit))
+                    doesItMatch = true;
+            }                
+        }
         //Then report the result here
+        
+        if(doesItMatch == true)
+            System.out.println("We've got a match");
+        else
+            System.out.println("Sorry no Match");
     }
     
 }
